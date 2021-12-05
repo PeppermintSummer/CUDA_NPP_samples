7a20e4237181900ca0b2ebf613aa91630520306a
peppermint <528129929@qq.com>
2021-12-05 12:16:11 +0800

    add cvtcolor yuv2bgr i420 reference

--- CUDA_NPP_samples/CUDA_color_conversion.cu
+++ CUDA_NPP_samples/CUDA_color_conversion.cu
@@ -102,3 +102,153 @@ int cvtColor(unsigned char *d_req,
 
 	return 0;
 }
+
+/*****************************************************************
+ *
+ *  cutColor yuv2bgr i420
+ */
+#define DESCALE(x, n)    (((x) + (1 << ((n)-1)))>>(n))
+#define COEFFS_0 		(22987)
+#define COEFFS_1 		(-11698)
+#define COEFFS_2 		(-5636)
+#define COEFFS_3 		(29049)
+#define clip(minv, maxv, value)  ((value)<minv) ? minv : (((value)>maxv) ? maxv : (value))
+__global__ void gpuConvertI420toBGR_kernel(
+	unsigned char *src_y, unsigned char *src_u, unsigned char *src_v, unsigned char *dst,
+	unsigned int width, unsigned int height)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx * 2 >= width) {
+		return;
+	}
+	int height_half = (height >> 1);
+	int width_half = (width >> 1);
+
+	for (int i = 0; i < height_half; ++i) {
+		int cb = src_u[i*width_half + idx];
+		int cr = src_v[i*width_half + idx];
+		int b = DESCALE((cb - 128)*COEFFS_3, 14);
+		int g = DESCALE((cb - 128)*COEFFS_2 + (cr - 128)*COEFFS_1, 14);
+		int r = DESCALE((cr - 128)*COEFFS_0, 14);
+		int y00 = src_y[(i * 2 + 0)*width + idx * 2 + 0];
+		int y01 = src_y[(i * 2 + 0)*width + idx * 2 + 1];
+		int y10 = src_y[(i * 2 + 1)*width + idx * 2 + 0];
+		int y11 = src_y[(i * 2 + 1)*width + idx * 2 + 1];
+
+		dst[(i * 2 + 0)*width * 3 + idx * 6 + 0] = clip(0, 255, y00 + b);//B
+		dst[(i * 2 + 0)*width * 3 + idx * 6 + 1] = clip(0, 255, y00 + g);//G
+		dst[(i * 2 + 0)*width * 3 + idx * 6 + 2] = clip(0, 255, y00 + r);//R
+		dst[(i * 2 + 0)*width * 3 + idx * 6 + 3] = clip(0, 255, y01 + b);//B
+		dst[(i * 2 + 0)*width * 3 + idx * 6 + 4] = clip(0, 255, y01 + g);//G
+		dst[(i * 2 + 0)*width * 3 + idx * 6 + 5] = clip(0, 255, y01 + r);//R
+		dst[(i * 2 + 1)*width * 3 + idx * 6 + 0] = clip(0, 255, y10 + b);//B
+		dst[(i * 2 + 1)*width * 3 + idx * 6 + 1] = clip(0, 255, y10 + g);//G
+		dst[(i * 2 + 1)*width * 3 + idx * 6 + 2] = clip(0, 255, y10 + r);//R
+		dst[(i * 2 + 1)*width * 3 + idx * 6 + 3] = clip(0, 255, y11 + b);//B
+		dst[(i * 2 + 1)*width * 3 + idx * 6 + 4] = clip(0, 255, y11 + g);//G
+		dst[(i * 2 + 1)*width * 3 + idx * 6 + 5] = clip(0, 255, y11 + r);//R
+	}
+}
+static cudaError_t cuConvert_yuv2bgr_i420(PCUOBJ pObj, Mat &dst)
+{
+	cudaError_t ret = cudaSuccess;
+	int width = dst.cols, height = dst.rows;
+
+	unsigned char *d_src = pObj->d_mem[pObj->imem];
+	unsigned char *d_dst = dst.data;
+
+	unsigned int blockSize = 1024;
+	unsigned int numBlocks = (width / 2 + blockSize - 1) / blockSize;
+
+	gpuConvertI420toBGR_kernel << <numBlocks, blockSize >> > (
+		d_src, d_src + width * height, d_src + width * height + ((width >> 1)*(height >> 1)), d_dst,
+		width, height);
+	cudaStreamSynchronize(NULL);
+
+	return ret;
+}
+
+cudaError_t cuConvertConn_yuv2bgr_i420(int chId, Mat &dst, int flag)
+{
+	//LOCK;
+	cudaError_t ret = cudaSuccess;
+	ret = cuConvert_yuv2bgr_i420(&gObjs[chId], dst);
+	if (ret != cudaSuccess) {
+		printf("%s(%i)  : cudaGetLastError() CUDA error: %d\n", __FILE__, __LINE__, (int)cudaGetLastError());
+	}
+	ret = cudaDeviceSynchronize();
+	//UNLOCK;
+	return ret;
+}
+
+//    Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y
+//    Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y
+//    Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y
+//    Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y      Y Y Y Y Y Y
+//    U U U U U U      V V V V V V      U V U V U V      V U V U V U
+//    V V V V V V      U U U U U U      U V U V U V      V U V U V U
+//    - I420 -          - YV12 -         - NV12 -         - NV21 -
+
+#define GPU_BLOCK_THREADS  512
+
+static dim3 gridDims(int numJobs) {
+	int numBlockThreads = numJobs < GPU_BLOCK_THREADS ? numJobs : GPU_BLOCK_THREADS;
+	return dim3(ceil(numJobs / (float)numBlockThreads));
+}
+
+static dim3 blockDims(int numJobs) {
+	return numJobs < GPU_BLOCK_THREADS ? numJobs : GPU_BLOCK_THREADS;
+}
+
+__global__ void convert_nv12_to_bgr_float_kernel(const uint8_t* y, const uint8_t* uv, int width, int height, int linesize,
+	H264Codec::CudaNorm norm, float* dst_b, float* dst_g, float* dst_r, int edge) {
+
+	int position = blockDim.x * blockIdx.x + threadIdx.x;
+	if (position >= edge) return;
+
+	int ox = position % width;
+	int oy = position / width;
+
+	const uint8_t& yvalue = y[oy * linesize + ox];
+	int offset_uv = oy / 2 * linesize + (ox / 2) * 2;
+	const uint8_t& u = uv[offset_uv + 0];
+	const uint8_t& v = uv[offset_uv + 1];
+
+	float b = 1.164f * (yvalue - 16.0f) + 2.018f * (u - 128.0f);
+	float g = 1.164f * (yvalue - 16.0f) - 0.813f * (v - 128.0f) - 0.391 * (u - 128.0f);
+	float r = 1.164f * (yvalue - 16.0f) + 1.596f * (v - 128.0f);
+
+	if (norm.type == H264Codec::CudaNormType::MeanSTD) {
+		dst_b[position] = (b * norm.scale - norm.mean[0]) / norm.std[0];
+		dst_g[position] = (g * norm.scale - norm.mean[1]) / norm.std[1];
+		dst_r[position] = (r * norm.scale - norm.mean[2]) / norm.std[2];
+	}
+	else if (norm.type == H264Codec::CudaNormType::ScaleAdd) {
+		dst_b[position] = b * norm.scale + norm.add;
+		dst_g[position] = g * norm.scale + norm.add;
+		dst_r[position] = r * norm.scale + norm.add;
+	}
+	else {
+		dst_b[position] = b;
+		dst_g[position] = g;
+		dst_r[position] = r;
+	}
+}
+
+namespace H264Codec {
+
+	void convert_nv12_to_bgr_float(const uint8_t* y, const uint8_t* uv, int width, int height, int linesize, const CudaNorm& norm, float* dst, cudaStream_t stream) {
+
+		int total = width * height;
+		auto grid = gridDims(total);
+		auto block = blockDims(total);
+
+		convert_nv12_to_bgr_float_kernel << <grid, block, 0, stream >> > (
+			y, uv, width, height, linesize, norm,
+			dst + width * height * 0,
+			dst + width * height * 1,
+			dst + width * height * 2,
+			total
+			);
+	}
+};
